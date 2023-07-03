f38acce21739fbc396c540daa5e03a3335b11a1a
Ibrahim-fci <ibrahimismail00000@gmail.com>
2023-07-03 12:01:03 +0300

    Update convertStrArrToIntArr.ts

--- src/utils/convertStrArrToIntArr.ts
+++ src/utils/convertStrArrToIntArr.ts
@@ -1,11 +1,18 @@
 const converter = async (arr: []) => {
-  let temp: number[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    temp.push(parseInt(arr[i]));
-  }
+  console.log(arr, "arr");
+  if (!arr) return [];
+  try {
+    let temp: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (!parseInt(arr[i])) continue;
+      temp.push(parseInt(arr[i]));
+    }
 
-  if (temp.length == 0) return [];
-  return temp;
+    if (temp.length == 0) return [];
+    return temp;
+  } catch {
+    return [];
+  }
 };
 
 export default converter;
